e53d94e6489372678ece11e9130aade983b389d2
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-09-09 10:29:39 +0530

    Create fifo_pkg.sv

---
+++ fifo_pkg.sv
@@ -0,0 +1,13 @@
+package fifo_pkg;
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+
+  `include "transaction.sv"
+  `include "driver.sv"
+  `include "monitor.sv"
+  `include "agent.sv"
+  `include "scoreboard.sv"
+  `include "environment.sv"
+  `include "basic_test_seq.sv"
+  `include "base_test.sv"
+endpackage
